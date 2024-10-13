9cf66fcc53476437b646c7c70416046d968e79bb
Oskarmle <121121527+Oskarmle@users.noreply.github.com>
2024-10-13 18:19:33 +0200

    start frontpage design

--- src/components/complex/startpage.jsx
+++ src/components/complex/startpage.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import "./complexComponents.css";
+import React from "react";
+
+// import props
+import instagramSVG from '../../svg_icons/instagram.svg'
+import githubSVG from '../../svg_icons/github.svg'
+import linkedinSVG from '../../svg_icons/linkedin.svg'
 
 export default function Startpage() {
   return (
-    <div className='startpage'>
-      <container>
-      <div className="personalInfo"></div>
-      <div className="intro"></div>
-      </container>
-      
+    <div className="startpage">
+      <div className="container">
+        <div className="personalInfo">
+          <div className="imgHeadshot">
+            <img src="/images/headshot.jpg" alt="" />
+          </div>
+          <div className="links">
+            <div className="linkedin socials">
+              <img src={linkedinSVG} alt="" />
+              <p>Linkedin</p>
+            </div>
+            <div className="instagram socials">
+              <img src={instagramSVG} alt="" />
+              <p>Instagram</p>
+            </div>
+            <div className="github socials">
+              <img src={githubSVG} alt="" />
+              <p>Github</p>
+            </div>
+          </div>
+        </div>
+        <div className="intro"></div>
+      </div>
     </div>
-  )
+  );
 }
